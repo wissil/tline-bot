dae12225390d85a4fff54b6eb71a234beb28fa45
Filip Kozjak <filip.kozjak@gmail.com>
2019-10-27 10:17:42 +0100

    Fix Heiking Ashi package name

--- tline-bot-algorithm/src/main/kotlin/com/betfair/tline/algorithm/heikinashi/models/HeikinAshiCandle.kt
+++ tline-bot-algorithm/src/main/kotlin/com/betfair/tline/algorithm/heikinashi/models/HeikinAshiCandle.kt
@@ -1,8 +1,8 @@
-package com.betfair.tline.algorithm.heikenashi.models
+package com.betfair.tline.algorithm.heikinashi.models
 
 import java.math.BigDecimal
 
-data class HeikenAshiCandle(
+data class HeikinAshiCandle(
     val open: BigDecimal,
     val close: BigDecimal,
     val high: BigDecimal,
